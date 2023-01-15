fba6a97bcc21c77beb7317cbe7ff1e66a7bc77c6
grayroom <leejh9226@gmail.com>
2023-01-14 16:58:51 -0800

    fix collectstatic

--- config/settings.py
+++ config/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/auth/static/'
+STATIC_ROOT = '/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist/auth/static'),
 ]
